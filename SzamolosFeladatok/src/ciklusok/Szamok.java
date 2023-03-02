74e8df432aeb2e0f580fbd6940a3c795b9af36f0
somogyiandrasistvan <somogyiandraska@gmail.com>
2023-03-02 13:22:48 +0100

    ujra

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -4,15 +4,15 @@ public class Szamok {
 
     public static void main(String[] args) {
         int szam3 = 2;
-        for (int index = 0; index < 2; index++) {
+        for (int index = 0; index < 3; index++) {
             int szam2 = 2;
-            for (int j = 0; j < 2; j++) {
+            for (int j = 0; j < 3; j++) {
                 int[] szamok = new int[3];
                 int szam = 2;
                 for (int i = 0; i < szamok.length; i++) {
                     szamok[i] += szam;
                     szam++;
-                    System.out.println(szam3+""+szam2 + "" + szamok[i]);
+                    System.out.println(szam3 + "" + szam2 + "" + szamok[i]);
                 }
                 szam2++;
             }
