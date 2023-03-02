df6197b6550e3259128b44d52dfa7435bfc8b29e
somogyiandrasistvan <somogyiandraska@gmail.com>
2023-03-02 14:18:01 +0100

    sarkanyok javitas

--- SzamolosFeladatok/src/ciklusok/Sarkanyok.java
+++ SzamolosFeladatok/src/ciklusok/Sarkanyok.java
@@ -6,14 +6,13 @@ public class Sarkanyok {
         int osszes = 145;
         int[] adatok = new int[20];
         for (int i = 1; i < 20; i++) {
-            for (int x = 0; x < 10; x++) {
+            for (int x = 1; x < 10; x++) {
                 for (int y = 1; y < 10; y++) {
-                    int elso = 7 * y;
-                    int masodik = 11 * x;
+                    int elso = 7 * x;
+                    int masodik = 11 * y;
                     adatok[i] += elso + masodik;
                     System.out.println(adatok[i]);
                 }
-                int y = 1;
             }
         }
     }
